1f0519f1b8638e15568ac4833df32f7ff1e142b8
Marta Kimsa <martusiak2712@gmail.com>
2021-03-13 13:51:08 +0100

    change default recipes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('chicken');
+  const [query, setQuery] = useState('banana');
 
   useEffect(() => {
     getRecipes()
